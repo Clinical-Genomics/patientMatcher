04d764ee28c7a6c48efe0b0ef131ed16c33fbd26
northwestwitch <rasi.chiara@gmail.com>
2019-01-13 08:44:36 +0100

    created a matching handler with test

---
+++ patientMatcher/match/handler.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import logging
+
+from patientMatcher.match.genotype_matcher import match as genomatch
+from patientMatcher.match.phenotype_matcher import match as phenomatch
+
+LOG = logging.getLogger(__name__)
+
+def database_matcher(database, patient_obj, max_pheno_score, max_geno_score):
+    """Handles a query patient matching against the database of patients
+
+    Args:
+        database(pymongo.database.Database)
+        patient_obj(dic): a mme formatted patient object
+        max_pheno_score(float): a number between 0 and 1
+        max_geno_score(float): a number between 0 and 1
+
+    Returns:
+        sorted_matches(list): a list of patient matches sorted by descending score
+    """
+    pheno_matches = []
+    geno_matches = []
+    matches = []
+
+    # phenotype score can be obtained if patient has an associated phenotype (HPO or OMIM terms)
+    if len(patient_obj['features']) or len(patient_obj['disorders']) > 0:
+        LOG.info('Matching phenotypes against database patients..')
+        pheno_matches = phenomatch(database, max_pheno_score, patient_obj['features'], patient_obj['disorders'])
+
+    # genomic score can be obtained if patient has at least one genomic feature
+    if len(patient_obj['genomicFeatures']) > 0:
+        LOG.info('Matching variants/genes against database patients..')
+        geno_matches = genomatch(database, patient_obj['genomicFeatures'], max_geno_score)
+
+    # obtain unique list of all patient IDs returned by the 2 algorithms:
+    pheno_m_keys = list(pheno_matches.keys())
+    geno_m_keys = list(geno_matches.keys())
+    unique_patients = list(set(pheno_m_keys + geno_m_keys))
+
+    # create matching result objects with combined score from the 2 algorithms
+    for key in unique_patients:
+        pheno_score = 0
+        geno_score = 0
+        patient_obj = None
+
+        if key in pheno_m_keys:
+            pheno_score = pheno_matches[key]['pheno_score']
+            patient_obj = pheno_matches[key]['patient_obj']
+
+        if key in geno_m_keys:
+            geno_score = geno_matches[key]['geno_score']
+            patient_obj = geno_matches[key]['patient_obj']
+
+        p_score = pheno_score + geno_score
+
+        match = {
+            'score' : {
+                'patient' : p_score,
+                'genotype' : geno_score,
+                'phenotype' : pheno_score
+            },
+            'patient' : patient_obj
+        }
+        matches.append(match)
+
+    # sort patient matches by patient (combined) score
+    sorted_matches = sorted(matches, key=lambda k : k['score']['patient'], reverse=True)
+
+    # return the highest scored matches only:
+    return sorted_matches
---
+++ tests/match/test_matching_handler.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from patientMatcher.utils.load import load_demo
+from patientMatcher.parse.patient import mme_patient
+from patientMatcher.match.handler import database_matcher as dbmatcher
+
+def test_combined_matching(demo_data_path, database, json_patients):
+    """Testing the combined matching algorithm"""
+
+    # load demo data in mock database
+    # load demo data to mock database using function located under utils/load
+    inserted_ids = load_demo(demo_data_path, database, False)
+    assert len(inserted_ids) == 50 # 50 test cases are loaded
+
+    # format test patient for query:
+    test_mme_patients = [ mme_patient(patient) for patient in json_patients ]
+
+    a_patient = test_mme_patients[0]
+    assert a_patient
+
+    matches = dbmatcher(database, a_patient, 0.5, 0.5)
+    assert len(matches) > 0
+
+    higest_scored_patient = matches[0]
+    lowest_scored_patient = matches[-1]
+
+    assert higest_scored_patient['score']['patient'] > lowest_scored_patient['score']['patient']
