fe152210b18e9560f5936548cd37314b74b5fdb7
northwestwitch <rasi.chiara@gmail.com>
2019-03-30 09:33:24 +0100

    fixed typo

--- patientMatcher/server/views.py
+++ patientMatcher/server/views.py
@@ -31,7 +31,7 @@ def add():
 
     match_external = False
     if controllers.get_nodes(database=current_app.db):
-        match_external = true
+        match_external = True
 
     # else import patient to database
     modified, inserted, matching_obj= backend_add_patient(mongo_db=current_app.db, patient=formatted_patient,
