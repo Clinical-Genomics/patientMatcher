543ac122f1b0b7472340f975e82bcb2dbabfc861
Tor Solli-Nowlan <tor.solli-nowlan@medisin.uio.no>
2019-04-02 11:07:26 +0200

    grep DB_* from instance/config.py for more dynamic creds

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,17 +1,23 @@
-#!/bin/bash
+#!/bin/bash -e
+
+CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [[ -z $(which python) ]]; then
     . /opt/conda/etc/profile.d/conda.sh
     conda activate patientMatcher
-    python -V
 fi
+
+# ugly grep, but lets user mount custom `instance/config.py` into image
+DB_USERNAME=$(grep '^DB_USERNAME =' $CURR_DIR/../instance/config.py | cut -f 3 -d' ' | tr -d \')
+DB_PASSWORD=$(grep '^DB_PASSWORD =' $CURR_DIR/../instance/config.py | cut -f 3 -d' ' | tr -d \')
+DB_NAME=$(grep '^DB_NAME =' $CURR_DIR/../instance/config.py | cut -f 3 -d' ' | tr -d \')
 
 SETUP_JS=/root/setup_mongo.js
 cat > $SETUP_JS << EOF
 conn = new Mongo();
-db = conn.getDB("pmatcher");
+db = conn.getDB("$DB_NAME");
 db.disableFreeMonitoring();
-db.createUser({user: "pmUser", pwd: "pmPassword", roles:["dbOwner"]});
+db.createUser({user: "$DB_USERNAME", pwd: "$DB_PASSWORD", roles:["dbOwner"]});
 EOF
 
 mongod --fork \
