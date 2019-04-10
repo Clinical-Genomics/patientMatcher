99b6672e814e86066622f4e37a3c4cb0a2d4dd0b
northwestwitch <rasi.chiara@gmail.com>
2019-04-10 13:15:26 +0200

    increase coverage

--- tests/match/test_matching_handler.py
+++ tests/match/test_matching_handler.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from patientMatcher.utils.add import load_demo
+from patientMatcher.utils.add import load_demo, backend_add_patient
 from patientMatcher.parse.patient import mme_patient
-from patientMatcher.match.handler import internal_matcher, save_async_response
+from patientMatcher.match.handler import internal_matcher, save_async_response, external_matcher
 
 def test_internal_matching(demo_data_path, database, json_patients):
     """Testing the combined matching algorithm"""
@@ -25,6 +25,25 @@ def test_internal_matching(demo_data_path, database, json_patients):
     lowest_scored_patient = matches[-1]
 
     assert higest_scored_patient['score']['patient'] > lowest_scored_patient['score']['patient']
+
+
+def test_external_matching(database, test_node, json_patients):
+    """Testing the function that trigger patient matching across connected nodes"""
+
+    patient = json_patients[0]
+
+    # insert test node object in database
+    database['nodes'].insert_one(test_node)
+
+    # insert patient object in database
+    inserted_ids = backend_add_patient(mongo_db=database, host='patientMatcher.host.se', patient=patient, match_external=False)
+    assert inserted_ids
+
+    ext_m_result = external_matcher(database, 'pmatcher', patient, test_node['_id'])
+    assert isinstance(ext_m_result, dict)
+    assert ext_m_result['data']['patient']['id'] == patient['id']
+    assert ext_m_result['has_matches'] == False
+    assert ext_m_result['match_type'] == 'external'
 
 
 def test_save_async_response(database, test_node):
