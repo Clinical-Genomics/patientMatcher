1d0711d35bda99546bbd487ce47e7608ad66ccfa
northwestwitch <rasi.chiara@gmail.com>
2019-04-23 12:01:28 +0200

    updated pheno files and introduce test option

--- patientMatcher/cli/update.py
+++ patientMatcher/cli/update.py
@@ -12,20 +12,29 @@ def update():
     pass
 
 @update.command()
-def resources():
+@click.option('--test',
+              help='Use this flag to test the function',
+              is_flag=True)
+def resources(test):
     """Updates HPO terms and disease ontology from the web.
     Specifically collect files from:
     http://purl.obolibrary.org/obo/hp.obo
     http://compbio.charite.de/jenkins/job/hpo.annotations/lastStableBuild/artifact/misc/phenotype_annotation.tab
     """
+    files = {}
     for key,item in PHENOTYPE_TERMS.items():
         url = item['url']
         destination = item['resource_path']
-        click.echo('Downloading {}..'.format(key))
         r = requests.get(url, stream=True)
-        with open(destination, 'wb') as f:
-            total_length = int(r.headers.get('content-length'))
+        total_length = int(r.headers.get('content-length'))
+        if test: # read file and get its size
+            files[key] = total_length # create an object for each downloadable file and save its length
+            if total_length:
+                click.echo('file {} found at the requested URL'.format(key))
+            continue
+        with open(destination, 'wb') as f: #overwrite file
             for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1):
                 if chunk:
                     f.write(chunk)
                     f.flush()
+    return files
