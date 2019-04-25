ff3a9d89cee6d08d55cc6e0fadda16e501d71063
Tor Solli-Nowlan <tor.solli-nowlan@medisin.uio.no>
2019-04-25 16:55:02 +0200

    make sure generating env matches running env

--- docker/update_env.sh
+++ docker/update_env.sh
@@ -1,23 +1,28 @@
 #!/bin/bash -e
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export DOCKER_CONDA_PREFIX=/opt/conda/envs/patientMatcher
-export DOCKER_ENV_NAME=patientMatcher
-TEMP_ENV_NAME="temp-${DOCKER_ENV_NAME}"
-PYTHON_VERSION=3.6.8
+# We're running this inside the previously built container to ensure
+# the OS/container environment matches what it will be run in.
 
-if [[ -z $(which conda) ]]; then
-    echo "You must have conda installed to update conda_env.yml"
-    exit 1
+if [[ -z $INSIDE_THE_CONTAINER ]]; then
+    docker run -e INSIDE_THE_CONTAINER=1 -v $DIR/..:/opt/patientMatcher --rm -it --entrypoint /opt/patientMatcher/docker/update_env.sh local/patientmatcher
+else
+    export DOCKER_CONDA_PREFIX=/opt/conda/envs/patientMatcher
+    export DOCKER_ENV_NAME=patientMatcher
+    TEMP_ENV_NAME="temp-${DOCKER_ENV_NAME}"
+    PYTHON_VERSION=3.6.8
+
+    # conda functions don't get exported to subshells, so source it
+    source /opt/conda/etc/profile.d/conda.sh
+
+    conda create -y --name $TEMP_ENV_NAME python=${PYTHON_VERSION}
+    conda activate $TEMP_ENV_NAME
+    conda install git -y
+    pip install -r $DIR/../requirements.txt -r $DIR/../requirements-dev.txt
+    conda env export --no-builds \
+        | perl -wlne 'if ($. == 1){print "name: $ENV{DOCKER_ENV_NAME}"} elsif (/^prefix:/){print "prefix: $ENV{DOCKER_CONDA_PREFIX}"} else {print}' \
+        > $DIR/conda_env.yml
+    conda deactivate
+    conda env remove --name $TEMP_ENV_NAME
 fi
-source $(conda info --base)/etc/profile.d/conda.sh
-
-conda create -y --no-default-packages --name $TEMP_ENV_NAME python=${PYTHON_VERSION}
-conda activate $TEMP_ENV_NAME
-conda install git -y --channel defaults --override-channels
-pip install -r $DIR/../requirements.txt -r $DIR/../requirements-dev.txt
-conda env export \
-    | perl -wlne '$IN_CHAN = (/^channels:/ || ($IN_CHAN && /^ /)) ? 1 : 0; if ($. == 1){print "name: $ENV{DOCKER_ENV_NAME}"} elsif ($IN_CHAN) {next} elsif (/^prefix:/){print "prefix: $ENV{DOCKER_CONDA_PREFIX}"} else {print}' \
-    > $DIR/conda_env.yml
-conda deactivate
-conda env remove --name $TEMP_ENV_NAME
