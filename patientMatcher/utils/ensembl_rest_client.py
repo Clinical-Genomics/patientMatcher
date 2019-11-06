cbec7cae7069ecf1581dd700d0dfbdc3cb345f1b
northwestwitch <rasi.chiara@gmail.com>
2019-11-06 16:13:18 +0100

    use ensembl rest API to convert ensemble gene to gene symbol

---
+++ patientMatcher/utils/ensembl_rest_client.py
@@ -0,0 +1,68 @@
+# -*- coding: UTF-8 -*-
+import json
+import logging
+import requests
+from urllib.error import HTTPError
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+LOG = logging.getLogger(__name__)
+
+HEADERS = {'Content-type':'application/json'}
+RESTAPI_37 = 'http://grch37.rest.ensembl.org'
+RESTAPI_38 = 'http://rest.ensembl.org/'
+PING_ENDPOINT = 'info/ping'
+
+BIOMART_37 =  "http://grch37.ensembl.org/biomart/martservice?query="
+BIOMART_38 =  "http://ensembl.org/biomart/martservice?query="
+
+class EnsemblRestApiClient:
+    """A class handling requests and responses to and from the Ensembl REST APIs.
+    Endpoints for human build 37: https://grch37.rest.ensembl.org
+    Endpoints for human build 38: http://rest.ensembl.org/
+    Documentation: https://github.com/Ensembl/ensembl-rest/wiki
+    doi:10.1093/bioinformatics/btu613
+    """
+
+    def __init__(self, build='37'):
+        if build == '38':
+            self.server = RESTAPI_38
+        else:
+            self.server = RESTAPI_37
+
+    def ping_server(self, server=RESTAPI_38):
+        """ping ensembl
+
+        Accepts:
+            server(str): default is 'https://grch37.rest.ensembl.org'
+
+        Returns:
+            data(dict): dictionary from json response
+        """
+        url = '/'.join([server, PING_ENDPOINT])
+        data = self.send_request(url)
+        return data
+
+    def send_request(self, url):
+        """Sends the actual request to the server and returns the response
+
+        Accepts:
+            url(str): ex. https://grch37.rest.ensembl.org/lookup/id/ENSG00000103591
+
+        Returns:
+            data(dict): dictionary from json response
+        """
+        data = {}
+        try:
+            request = Request(url, headers=HEADERS)
+            response = urlopen(request)
+            content = response.read()
+            if content:
+                data = json.loads(content)
+        except HTTPError as e:
+            LOG.info('Request failed for url {0}: Error: {1}\n'.format(url, e))
+            data = e
+        except ValueError as e:
+            LOG.info('Request failed for url {0}: Error: {1}\n'.format(url, e))
+            data = e
+        return data
---
+++ tests/utils/test_ensembl_rest_api.py
@@ -0,0 +1,38 @@
+# -*- coding: UTF-8 -*-
+import tempfile
+from urllib.error import HTTPError
+from urllib.parse import urlencode
+from patientMatcher.utils import ensembl_rest_client as ensembl_api
+
+def test_ping_ensemble_37():
+    """Test ping ensembl server containing human build 37"""
+    client = ensembl_api.EnsemblRestApiClient()
+    data = client.ping_server()
+    assert data == {'ping':1}
+
+def test_ping_ensemble_38():
+    """Test ping ensembl server containing human build 38"""
+    client = ensembl_api.EnsemblRestApiClient(build='38')
+    data = client.ping_server()
+    assert data == {'ping':1}
+
+def test_send_gene_request():
+    """Test send request with correct params and endpoint"""
+    url = 'https://grch37.rest.ensembl.org/lookup/id/ENSG00000103591'
+    client = ensembl_api.EnsemblRestApiClient()
+    data = client.send_request(url)
+    # get info for the ensembl gene
+    assert data['display_name'] == 'AAGAB'
+
+def test_send_request_wrong_url():
+    """Successful requests are tested by other tests in this file.
+       This test will trigger errors instead.
+    """
+    url = 'fakeyurl'
+    client = ensembl_api.EnsemblRestApiClient()
+    data = client.send_request(url)
+    assert type(data) == ValueError
+
+    url = 'https://grch37.rest.ensembl.org/fakeyurl'
+    data = client.send_request(url)
+    assert type(data) == HTTPError
