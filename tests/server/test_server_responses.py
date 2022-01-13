0d1b298a17bf95be9e9895bd1c9f0378d46039cc
Chiara Rasi <rasi.chiara@gmail.com>
2022-01-13 11:14:14 +0100

    fixed one more test

--- tests/server/test_server_responses.py
+++ tests/server/test_server_responses.py
@@ -195,7 +195,7 @@ def test_add_patient_malformed_data(mock_app, test_client, gpx4_patients, test_n
 def test_add_patient(mock_app, test_client, gpx4_patients, test_node, database):
     """Test adding a patient by sending a POST request to the add endpoint with valid data"""
 
-    # GIVEN a mocked Ensembl REST API:
+    # GIVEN a mocked Ensembl REST API converting gene symbol to Ensembl ID
     responses.add(
         responses.GET,
         f"https://grch37.rest.ensembl.org/xrefs/symbol/homo_sapiens/GPX4?external_db=HGNC",
@@ -468,8 +468,39 @@ def test_patient_matches(mock_app, database, match_objs, test_client):
     assert len(matches) == 2
 
 
+@responses.activate
 def test_match_hgnc_symbol_patient(mock_app, gpx4_patients, test_client, database):
-    """Testing matching patient with gene symbl against patientMatcher database (internal matching)"""
+    """Testing matching patient with gene symbol against patientMatcher database (internal matching)"""
+
+    # GIVEN a mocked Ensembl REST API converting gene symbol to Ensembl ID
+    responses.add(
+        responses.GET,
+        f"https://grch37.rest.ensembl.org/xrefs/symbol/homo_sapiens/GPX4?external_db=HGNC",
+        json=[{"id": "ENSG00000167468", "type": "gene"}],
+        status=200,
+    )
+
+    # GIVEN a mocked Ensembl gene lookup service:
+    responses.add(
+        responses.GET,
+        f"https://grch37.rest.ensembl.org/lookup/id/ENSG00000167468",
+        json=[{"display_name": "GPX4"}],
+        status=200,
+    )
+
+    # GIVEN a mocked liftover service:
+    responses.add(
+        responses.GET,
+        f"https://grch37.rest.ensembl.org/map/human/GRCh37/19:1105813..1105814/GRCh38?content-type=application/json",
+        json=[],
+        status=200,
+    )
+    responses.add(
+        responses.GET,
+        f"https://grch37.rest.ensembl.org/map/human/GRCh37/19:1106232..1106238/GRCh38?content-type=application/json",
+        json=[],
+        status=200,
+    )
 
     # add an authorized client to database
     ok_token = test_client["auth_token"]
